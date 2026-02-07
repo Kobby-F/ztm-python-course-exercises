a1c61f692ddd06fbf933f040b51259baf106bee1
Kobby <kfrimpong0712@gmail.com>
2026-02-07 16:11:17 -0600

    Add f-string formatting for balance message

--- 2. Python Basics/3-ExerciseFormattedStrings.py
+++ 2. Python Basics/3-ExerciseFormattedStrings.py
@@ -12,7 +12,7 @@ print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
 # 2 How would you write this using f-string? (Scroll down for answer)
 
 
-
+print(f"Hello {name}, your balance is {amount}")
 
 
 
